d0ac9a773eb675c8bdce47d59c1239ccb408a80c
CH13G <1120868240@qq.com>
2019-11-15 15:17:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## font-test
+> node >=8 
+
+## 安装依赖
+
+
+ ```bash
+  # 如果尝试用js 转文字则需要安装 tesseract
+  $ brew install tesseract
+
+  $ npm install
+  ```
+
+## 运行
+```bash
+    $ node ./index.js
+```
---
+++ index.js
@@ -0,0 +1,112 @@
+const fs = require('fs');
+const path = require('path');
+const fontCarrier = require('font-carrier');
+const _ = require('lodash');
+const fontBlast = require('font-blast')
+const svg2img = require('svg2img');
+const util = require('util');
+const tesseract = require('tesseractocr');
+const readdirp = require('readdirp');
+const pLimit = require('p-limit');
+const makeDir = require('make-dir');
+
+const _cliProgress = require('cli-progress');
+const _colors = require('colors');
+
+const readFileAsync = util.promisify(fs.readFile);
+const svg2imgAsync = util.promisify(svg2img);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
+async function delay(time = 1000) {
+  return new Promise(r => {
+    setTimeout(r, time);
+  })
+}
+
+async function genOutDir() {
+  const OUTPUT = 'dist';
+  const pathDir = await makeDir(OUTPUT);
+  return pathDir;
+}
+
+async function genSvg(file, color = '#000') {
+  const { name } = path.parse(file);
+  const outdir = await genOutDir();
+  const outFile = path.join(outdir, `${name}.svg`);
+  const transFont = fontCarrier.transfer(file);
+  await writeFileAsync(outFile, transFont.toString())
+  fontBlast(outFile, name);
+  const files = await readdirp.promise(`${name}/svg`, { fileFilter: '*.svg' });
+  const limit = pLimit(5);
+  const results = await Promise.all(files.map(fStat =>
+    limit(() =>
+      readFileAsync(fStat.fullPath, 'utf8')
+        .then(str => {
+          const fixStr = str.replace(/viewBox=\"0 0 1000 1000\"/, 'viewBox=\"-300 100 1500 1500\"').replace(/\<path/, `<path fill="${color}"`)
+          fs.writeFileSync(fStat.fullPath, fixStr)
+          return fStat
+        })
+    )
+  ));
+  return results
+}
+
+async function transFont(file = './font_926539_x9cng5u1o6m/8ed29038.woff', format = 'png') {
+  const files = await genSvg(file);
+  const limit = pLimit(5);
+  const { name } = path.parse(file);
+  const baseDir = `${name}/${format}`;
+  const pathDir = await makeDir(baseDir);
+  const results = await Promise.all(files.map(fStat => 
+    limit(() => 
+      svg2imgAsync(fStat.fullPath, { format, })
+        .then(buffer => {
+          const filename = path.join(pathDir, fStat.basename.replace(/svg$/, format));
+          fs.writeFileSync(filename, buffer)
+          return filename
+        })
+    )
+    ));
+  console.log('transFont done');
+  return results;
+}
+
+async function imageToText(files = [], fontFile) {
+  // const list = await tesseract.listLanguages();
+  const b1 = new _cliProgress.SingleBar({
+    format: 'imageToText Progress |' + _colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+  });
+  b1.start(files.length, 0, {
+    speed: "N/A"
+  });
+  const recognize = tesseract.withOptions({
+    language: ['chi_sim']
+  });
+  const limit = pLimit(5);
+  const result = {};
+  await Promise.all(files.map(file => limit(() => recognize(file).then(text => {
+    const { name } = path.parse(file);
+    result[name] = text;
+    b1.increment();
+  }))));
+  b1.stop();
+  const outdir = await genOutDir();
+  const outFile = path.join(outdir, `${path.parse(fontFile).name}.json`);
+  fs.writeFileSync(outFile, JSON.stringify(result));
+  console.log(result);
+}
+
+async function init(file = './font_926539_x9cng5u1o6m/8ed29038.woff') {
+  const files = await transFont(file);
+  await imageToText(files, file)
+}
+// 只转化图片
+transFont('./font_926539_x9cng5u1o6m/8ed29038.woff', 'png')
+
+// 转化图片 且 尝试node 文字提取
+// init('./font_926539_x9cng5u1o6m/8ed29038.woff')
